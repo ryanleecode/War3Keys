5b17a37a4a240ed0da3277d89684d668ffbf7181
Unknown <drdgvhbh@gmail.com>
2018-08-04 12:11:11 -0400

    Update packages and config

--- webpack.renderer.additions.js
+++ webpack.renderer.additions.js
@@ -1,6 +1,16 @@
+const path = require('path');
+
 module.exports = {
   watchOptions: {
     aggregateTimeout: 5000,
     poll: 10000
+  },
+  output: {
+    pathinfo: false
+  },
+  resolve: {
+    alias: {
+      '@renderer': path.resolve(__dirname, 'src/renderer/')
+    }
   }
 }
